ae83fd5ade97eb48101eeef0a1d073b8501a9c6a
Hackbright Student <info@hackbrightacademy.com>
2015-07-08 13:09:35 -0700

    n-grams in progress

--- markov.py
+++ markov.py
@@ -2,7 +2,7 @@ import sys
 import random
 
 
-def make_chains(corpus):
+def make_chains(corpus, n=2):
     """Takes input text as string; returns dictionary of markov chains."""
     text_file = open(corpus)
     full_text_string = []
@@ -12,30 +12,36 @@ def make_chains(corpus):
     
     #print full_text_string
     
-    length_full_text = len(full_text_string)-2
+    length_full_text = len(full_text_string)-n
     ngrams = {}
     for i in range(length_full_text):
-        tup = (full_text_string[i], full_text_string[i+1])
-        ngrams.setdefault(tup,[]).append(full_text_string[i+2])
+        pre_tup = []
+        for j in range(n):
+            pre_tup.append(full_text_string[i+j])
+        tup = tuple(pre_tup)
+        ngrams.setdefault(tup,[]).append(full_text_string[i+n])
 
     return ngrams
 
 #green_eggs = make_chains("green-eggs.txt")
 #print green_eggs
-def make_text(chains):
+def make_text(chains, n=2):
     """Takes dictionary of markov chains; returns random text."""
     start = random.choice(chains.keys())
-    first_word = start[-1]
+    first_word = list(start[-(n-1):]) 
     next = random.choice(chains[start])
     result = list(start)
     result.append(next)
-    #print " ".join(result)
 
-    while next[-1] != ".":
-        new = tuple([first_word, next])
-        next = random.choice(chains[new])
-        result.append(next)
-        first_word = result[-2]
+
+    while next[-1].isalpha():
+        new = tuple(first_word+[next]) #should be list converted to tuple--make sure it create n tuple
+        print new
+        next_word = random.choice(chains[new]) #check to see if changing variable "next" works
+        
+        print "This is next_word: ", next_word
+        result.append(next_word)
+        first_word = result[-n:-(n-1)] #check index--work here!
         next = result[-1]
 
 
@@ -57,9 +63,9 @@ text = sys.argv[1]
 input_text = text
 
 # # Get a Markov chain
-chain_dict = make_chains(input_text)
+chain_dict = make_chains(input_text, 3)
 
 # # Produce random text
-random_text = make_text(chain_dict)
+random_text = make_text(chain_dict, 3)
 
 print random_text
